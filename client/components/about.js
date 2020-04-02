50683434b2b04bc73ef47ac62dd1c78efe2e3423
petesamper <petesamper@gmail.com>
2020-04-02 16:05:39 -0400

    added my profile section

--- client/components/about.js
+++ client/components/about.js
@@ -56,57 +56,42 @@ const about = () => {
 
             <div className="elisCard" id="leftCard about-card">
               <div className="smallTitleLeft">Peter</div>
-              <div className="smallSubTitle">American actor and producer</div>
-              <img
-                className="compare-img"
-                src="https://upload.wikimedia.org/wikipedia/commons/d/d0/TomHanksJan2009.jpg"
-              />
+              <div className="smallSubTitle">Software engineer</div>
+              <img className="compare-img" src="/img/Pete_Photo.jpg" />
               <div className="smallPictureDescription">
-                <a
-                  href="https://en.wikipedia.org/wiki/Tom_Hanks"
-                  className="extiw"
-                  title="en:Tom Hanks"
-                >
-                  Tom Hanks
-                </a>{' '}
-                recites the orchestral work "Lincoln Portrait," written by Aaron
-                Copland, at the Lincoln Memorial on the National Mall in
-                Washington, D.C., January 18, 2009, during the inaugural opening
-                ceremonies. More than 5,000 men and women in uniform are
-                providing military ceremonial support to the presidential
-                inauguration for Barack Obama, a tradition dating back to George
-                Washington's 1789 inauguration.
+                Developer | Veteran | Coffee enthusiast
               </div>
               <div className="smallInfo">
                 <div className="smallInfoTitleLeft">Information</div>
                 <div className="smallInfoMain">
                   <div className="theSmalls">
-                    <div className="smallInfoH">Commons category</div>
-                    <div className="smallInfoD">Tom Hanks</div>
-                  </div>
-                  <div className="theSmalls">
-                    <div className="smallInfoH">VIAF ID</div>
-                    <div className="smallInfoD">19874894</div>
-                  </div>
-                  <div className="theSmalls">
-                    <div className="smallInfoH">
-                      International Standard Name Identifier
-                    </div>
-                    <div className="smallInfoD">0000 0001 2123 2378</div>
-                  </div>
-                  <div className="theSmalls">
-                    <div className="smallInfoH">occupation</div>
+                    <div className="smallInfoH">operating system used</div>
                     <div className="smallInfoD">
-                      film actor, film director, actor, film producer, writer,
-                      voice actor, screenwriter, composer, character actor,
-                      television actor, television director, television producer
+                      <a href="https://www.wikidata.org/wiki/Q43627">macOS</a>
                     </div>
                   </div>
                   <div className="theSmalls">
-                    <div className="smallInfoH">country of citizenship</div>
+                    <div className="smallInfoH">languages spoken or signed</div>
                     <div className="smallInfoD">
-                      United States of America, Greece
+                      <a href="https://www.wikidata.org/wiki/Q1860">English</a>
                     </div>
+                  </div>
+                  <div className="theSmalls">
+                    <div className="smallInfoH">Favorite Game:</div>
+                    <div className="smallInfoD">
+                      Everything made by{' '}
+                      <a href="https://www.wikidata.org/wiki/Q2414469">
+                        FromSoftware
+                      </a>
+                    </div>
+                  </div>
+                  <div className="theSmalls">
+                    <div className="smallInfoH">Height</div>
+                    <div className="smallInfoD">6'</div>
+                  </div>
+                  <div className="theSmalls">
+                    <div className="smallInfoH">shoe size</div>
+                    <div className="smallInfoD">11</div>
                   </div>
                 </div>
               </div>
