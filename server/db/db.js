f510b260354c50330b099d86244d7ee7bfc54a44
Harry P <habs@sdf.org>
2021-05-18 14:47:43 -0400

    update db

--- server/db/db.js
+++ server/db/db.js
@@ -1,18 +1,28 @@
-const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
+const Sequelize = require('sequelize');
+const pkg = require('../../package.json');
 
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const prod = process.env.NODE_ENV !== 'test';
+
+const databaseName = pkg.name + (!prod ? '-test' : '');
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false
+    ssl: prod,
+    dialectOptions: prod
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      : {}
   }
-)
-module.exports = db
+);
+module.exports = db;
 
 // This is a global Mocha hook used for resource cleanup.
 // Otherwise, Mocha v4+ does not exit after tests.
 if (process.env.NODE_ENV === 'test') {
-  after('close database connection', () => db.close())
+  after('close database connection', () => db.close());
 }
